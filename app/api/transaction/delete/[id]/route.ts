3162a97a4c8992c092f1cfee6c47803d30289a80
GerEspinosa <espinosa.toscano@gmail.com>
2025-05-29 20:36:27 +0200

    FInal Version - Update DELETE

--- app/api/transaction/delete/[id]/route.ts
+++ app/api/transaction/delete/[id]/route.ts
@@ -2,11 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 import Transaction from "../../../../models/Transaction";
 import { db } from "@/app/lib/db";
 
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
-  const transactionId = params?.id;
+export async function DELETE(req: NextRequest) {
+  const url = new URL(req.url);
+  const transactionId = url.pathname.split("/").pop(); // extrae el id de la URL
+
   if (!transactionId) {
     return NextResponse.json({ error: "No transaction id provided" }, { status: 400 });
   }
+
   try {
     await db();
     const transaction = await Transaction.findByIdAndDelete(transactionId);
